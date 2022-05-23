5f37fb9de1b6edc02225901fd9960b8ce679ea8f
Kat Taylor <53796272+CoralStar@users.noreply.github.com>
2022-05-23 09:22:56 -0500

    Added in Consts
    Step 1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const guessedLetters = document.QuerySelector(".guessed-letters");
+const guessedLettersButton = document.QuerySelector(".guess");
+const letterInput = document.QuerySelector(".letter");
+const wordProgress = document.QuerySelector(".word-in-progress");
+const guessesRemaining = document.QuerySelector(".remaining");
+const guessesRemainingSpan = document.QuerySelector(".remaining span");
+const message = document.QuerySelector(".message");
+const playAgainButton = document.QuerySelector(".play-again hide");
