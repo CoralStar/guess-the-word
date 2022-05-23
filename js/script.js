c22bd5efbe45b295b1040b45b966a609815bf227
Kat Taylor <53796272+CoralStar@users.noreply.github.com>
2022-05-23 09:33:21 -0500

    Added Function
    Step 2

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ const guessesRemaining = document.QuerySelector(".remaining");
 const guessesRemainingSpan = document.QuerySelector(".remaining span");
 const message = document.QuerySelector(".message");
 const playAgainButton = document.QuerySelector(".play-again hide");
+
+const word = "magnolia";
+
+const dots = function (word) {
+  const dotsLetters = [];
+  for (const letter of word) {
+  console.log(letter);
+  dotsLetters.push("●");
+  }
+ wordProgress.innerText = dotsLetters.join("");
+};
